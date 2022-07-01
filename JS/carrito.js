8494aeef6069a441c3619280e19c3464b2b7521a
Nicolas Heguaburu <nicoheguaburu@gmail.com>
2022-07-01 14:30:15 -0300

    carrito style

--- JS/carrito.js
+++ JS/carrito.js
@@ -20,7 +20,7 @@ function dibujarCarrito() {
     arrayCarrito.forEach(item => {
         const divCarritoItem = document.createElement('div');
         divCarritoItem.innerHTML =
-            `<ul class="d-flex flex-row" id="listaCarritoItem"><li>${item.nombre}</li><li>${item.precio}  </li><li>${item.cantidad}</li></ul>`
+            `<ul class="d-flex" id="listaCarritoItem"><li>${item.nombre}</li><li>${item.precio}  </li><li>${item.cantidad}</li></ul>`
         cuerpoCarrito.append(divCarritoItem);
     });
     let precioTotal = 0;
@@ -28,7 +28,7 @@ function dibujarCarrito() {
         precioTotal = item.precio + precioTotal;
     });
     const divTotal = document.createElement('div');
-    divTotal.innerHTML = `<h6 class="d-flex justify-content-center">TOTAL: ${precioTotal}</h6><button onclick="vaciarCarrito()">vaciar carro</button>`
+    divTotal.innerHTML = `<h6 class="d-flex justify-content-center mt-1">TOTAL: ${precioTotal}</h6><button id="btnVaciarCarro" onclick="vaciarCarrito()">vaciar carro</button>`
     cuerpoCarrito.append(divTotal);
 }
 
