85d761369e172260050e9b58bfad7dd347dc9558
Nicolas Heguaburu <nicoheguaburu@gmail.com>
2022-07-19 17:18:58 -0300

    fetch en cajas Y emailJS con apk

--- JS/cajas.js
+++ JS/cajas.js
@@ -1,25 +1,39 @@
 const cartaCajas = document.querySelector('#divCajas');
 
-const stockCajas = [
-    { id: 2, tipo: 'cajas', nombre: 'cajaB', precio: 2000, cantidad: 0, precioXcantidad: 0, },
-    { id: 1, tipo: 'cajas', nombre: 'cajaA', precio: 1000, cantidad: 0, precioXcantidad: 0, },
-    { id: 3, tipo: 'cajas', nombre: 'cajaC', precio: 1200, cantidad: 0, precioXcantidad: 0, },
-    { id: 4, tipo: 'cajas', nombre: 'cajaD', precio: 1100, cantidad: 0, precioXcantidad: 0, },
-    { id: 5, tipo: 'cajas', nombre: 'cajaE', precio: 2400, cantidad: 0, precioXcantidad: 0, },
-    { id: 6, tipo: 'cajas', nombre: 'cajaF', precio: 2100, cantidad: 0, precioXcantidad: 0, }
-]
+// const stockCajas = [
+//     { id: 2, tipo: 'cajas', nombre: 'cajaB', precio: 2000, cantidad: 0, precioXcantidad: 0, },
+//     { id: 1, tipo: 'cajas', nombre: 'cajaA', precio: 1000, cantidad: 0, precioXcantidad: 0, },
+//     { id: 3, tipo: 'cajas', nombre: 'cajaC', precio: 1200, cantidad: 0, precioXcantidad: 0, },
+//     { id: 4, tipo: 'cajas', nombre: 'cajaD', precio: 1100, cantidad: 0, precioXcantidad: 0, },
+//     { id: 5, tipo: 'cajas', nombre: 'cajaE', precio: 2400, cantidad: 0, precioXcantidad: 0, },
+//     { id: 6, tipo: 'cajas', nombre: 'cajaF', precio: 2100, cantidad: 0, precioXcantidad: 0, }
+// ]
 
-stockCajas.forEach((item) => {
-    const divCajas = document.createElement('div');
-    divCajas.classList.add('cartaProductos')
-    divCajas.innerHTML = `<div class="imgPorAhora"></div>
+fetch('https://mocki.io/v1/d47323dd-ac3a-47b6-9191-aeadb21f7de1')
+    .then(response => response.json())
+    .then(data => crearStock(data))
+
+const crearStock = (Data) => {
+    stockCajas = Data
+
+
+    stockCajas.forEach((item) => {
+        const divCajas = document.createElement('div');
+        divCajas.classList.add('cartaProductos')
+        divCajas.innerHTML = `<div class="imgPorAhora"></div>
                 <h3 class="tituloProductos">${item.nombre}</h3>
                 <h3 class="precioProductos">${item.precio}$</h3>
                 <p class="descripcionProductos">descripcion:</p>
                 <button type="button" onclick="agregarAlCarritoCajas(${item.id})" class="btn btn-success w-100 agragarCarrito" data-bs-toggle="button">Agregar <i class="bi bi-cart-plus-fill"></i></button>`
 
-    cartaCajas.append(divCajas);
-});
+        cartaCajas.append(divCajas);
+    });
+}
+
+
+
+
+
 
 const agregarAlCarritoCajas = (id) => {
     const item = stockCajas.find((producto) => producto.id === id)
--- JS/contacto.js
+++ JS/contacto.js
@@ -4,66 +4,18 @@ const btn = document.getElementById('button');
 // FUNCIONANDO
 // SDK DE LA API
 
-// document.getElementById('form')
-//     .addEventListener('submit', function(event) {
-//         event.preventDefault();
-
-//         btn.value = 'Enviando...';
-
-//         const serviceID = 'default_service';
-//         const templateID = 'template_tw5rsrj';
-
-//         emailjs.sendForm(serviceID, templateID, this)
-//             .then(() => {
-//                 btn.value = 'Send Email';
-//                 Toastify({
-//                     text: "Mensaje Enviado",
-//                     duration: 2000,
-//                     gravity: 'top',
-//                     position: 'right',
-//                     style: {
-//                         background: '#198754'
-//                     }
-//                 }).showToast();
-//             }, (err) => {
-//                 btn.value = 'Send Email';
-//                 alert(JSON.stringify(err));
-//             });
-//     });
-
-
-
-
-
-
-
-
-
-// NO FUNCIONANDO
-// CON FETCH
-
 document.getElementById('form')
     .addEventListener('submit', function(event) {
-        event.preventDefault(); // prevent reload
-        btn.value = 'enviando..'
+        event.preventDefault();
 
-        let Data = new FormData(this);
+        btn.value = 'Enviando...';
 
-        Data.append('service_id', 'default_service');
-        Data.append('template_id', 'template_tw5rsrj');
-        Data.append('user_id', 'J9fRhPVVBUpKg1wtM');
+        const serviceID = 'default_service';
+        const templateID = 'template_tw5rsrj';
 
-
-        fetch('https://api.emailjs.com/api/v1.0/email/send-form', {
-                method: 'POST',
-                body: JSON.stringify(Data),
-                headers: {
-                    contentType: false,
-                    processData: false
-                }
-            })
+        emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
-                btn.value = 'Enviar mensaje';
+                btn.value = 'Send Email';
                 Toastify({
                     text: "Mensaje Enviado",
                     duration: 2000,
@@ -72,40 +24,9 @@ document.getElementById('form')
                     style: {
                         background: '#198754'
                     }
-                });
+                }).showToast();
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
             });
-    })
-
-
-
-
-
-
-
-
-
-
-// DOCUMENTACION DE LA API 
-// CON AJAX
-
-
-
-// $('#myForm').on('submit', function(event) {
-//     event.preventDefault(); // prevent reload
-
-//     var formData = new FormData(this);
-//     formData.append('service_id', 'YOUR_SERVICE_ID');
-//     formData.append('template_id', 'YOUR_TEMPLATE_ID');
-//     formData.append('user_id', 'YOUR_PUBLIC_KEY');
-
-//     $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
-//         type: 'POST',
-//         data: formData,
-//         contentType: false, // auto-detection
-//         processData: false // no need to parse formData to string
-//     }).done(function() {
-//         alert('Your mail is sent!');
-//     }).fail(function(error) {
-//         alert('Oops... ' + JSON.stringify(error));
-//     });
-// });
+    });
